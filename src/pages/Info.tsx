6b8fa34b5b41229f4e33b71d2a1fede1a6648d1d
dya-code <entrydia7691@gmail.com>
2022-12-20 05:12:42 +0900

    test api

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,14 +1,34 @@
-import { SetStateAction, useState } from 'react'
+import { SetStateAction, useEffect, useState } from 'react'
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 function Info() {
   const school_name = sessionStorage.getItem("schoolName")
+  let res = []
+  
+  let schoolName = ''
+  let engName = ''
+
+  useEffect(() => {
+    const GetSchoolInfo = async () => {
+      await axios.get(`https://open.neis.go.kr/hub/schoolInfo?SCHUL_NM=${ school_name }&Type=json&pIndex=1&pSize=100&KEY=d374573af8d34cddaf4e4c250b995c8c`)
+        .then(resp => res = resp.data.schoolInfo[1].row)
+
+      // schoolName = res.SCHUL_NM
+      // engName = res.ENG_SCHUL_NM
+    }
+    GetSchoolInfo()
+
+    console.log(res)
+    
+  }, [])
 
   return (
     <div className="info flex items-center justify-center">
-      <div className="info text-neutral-100 text-2xl">{ school_name }</div>
+      <div className="text-neutral-100 text-2xl">{ schoolName }</div>
+      <div className="text-neutral-200 text-1xl">{ engName }</div>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/hub': {
+        target: 'https://open.neis.go.kr',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/hub/, ''),
+        secure: false,
+        ws: true
+      }
+    }
+  }
 })

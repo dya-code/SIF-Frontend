8e415b45cf4a044a76957477b1ed47ef4ae1fc67
dya-code <dyacode@proton.me>
2022-12-23 22:29:51 +0900

    school name

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,34 +1,26 @@
 import { SetStateAction, useEffect, useState } from 'react'
 import axios from 'axios'
 import reactLogo from './assets/react.svg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
 
 function Info() {
   const school_name = sessionStorage.getItem("schoolName")
-  let res = []
-  
-  let schoolName = ''
-  let engName = ''
+  const [res, setRes] = useState<any>([])
+  // const [infoArr, setInfoArr] = useState<{name: string; engname: number}[]>([]);
 
   useEffect(() => {
     const GetSchoolInfo = async () => {
       await axios.get(`https://open.neis.go.kr/hub/schoolInfo?SCHUL_NM=${ school_name }&Type=json&pIndex=1&pSize=100&KEY=d374573af8d34cddaf4e4c250b995c8c`)
-        .then(resp => res = resp.data.schoolInfo[1].row)
-
-      // schoolName = res.SCHUL_NM
-      // engName = res.ENG_SCHUL_NM
+        .then(resp => { setRes(resp.data.schoolInfo[1].row[0]); console.log(res) })
     }
-    GetSchoolInfo()
 
-    console.log(res)
-    
+    GetSchoolInfo()
   }, [])
 
   return (
-    <div className="info flex items-center justify-center">
-      <div className="text-neutral-100 text-2xl">{ schoolName }</div>
-      <div className="text-neutral-200 text-1xl">{ engName }</div>
+    <div className="info flex items-center justify-center flex-col">
+      <div className="text-neutral-100 text-2xl">{ res.SCHUL_NM }</div>
+      <div className="text-neutral-200 text-1xl">{ res.ENG_SCHUL_NM }</div>
     </div>
   )
 }
